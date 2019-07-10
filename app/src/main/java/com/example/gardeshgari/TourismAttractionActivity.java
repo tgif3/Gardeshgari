f67eb132879876973376566ee8eb0d129c3cd877
Hossein <hbehboudi@ce.sharif.edu>
2019-07-10 14:14:28 +0430

    ریفکتور

--- app/src/main/java/com/example/gardeshgari/TourismAttractionActivity.java
+++ app/src/main/java/com/example/gardeshgari/TourismAttractionActivity.java
@@ -1,9 +1,14 @@
 package com.example.gardeshgari;
 
+import android.content.Context;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.gardeshgari.Model.AttractionModel;
 import com.example.gardeshgari.adapter.HorizontalAdapter;
@@ -17,14 +22,11 @@ import me.relex.circleindicator.CircleIndicator;
 
 public class TourismAttractionActivity extends AppCompatActivity {
 
-    private HorizontalListView horizontalListView;
-    private HorizontalAdapter horizontalAdapter;
-    private ArrayList<AttractionModel> attractionModelArrayList;
-
-
     private static ViewPager mPager;
     private static int currentPage = 0;
     private ArrayList<String> urls = new ArrayList<>();
+    private static LayoutInflater layoutInflater = null;
+    private ArrayList<HorizontalListView> horizontalListViews;
 
 
     @Override
@@ -32,27 +34,45 @@ public class TourismAttractionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourism_attraction);
 
-        updateData();
+        horizontalListViews = new ArrayList<>();
+        layoutInflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        horizontalListView = findViewById(R.id.HorizontalListView);
-        horizontalAdapter = new HorizontalAdapter(this, attractionModelArrayList);
-        horizontalListView.setAdapter(horizontalAdapter);
+        ArrayList<AttractionModel> attractionModelArrayList1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            attractionModelArrayList1.add(new AttractionModel("جنگل سی سنگان نوشهر",
+                    "https://www.zistonline.com/uploadFiles/editor/Alangdareh-park.jpg"));
+        }
+        createListView(attractionModelArrayList1, "جنگل‌های سرسبز شمال کشور");
+
+        ArrayList<AttractionModel> attractionModelArrayList2 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            attractionModelArrayList2.add(new AttractionModel("جنگل حاشیه‌ی دریای خزر",
+                    "https://cdn.mehrbooking.net/general/Images/Blog/fa/2017/06/beautiful-forests-of-the-caspian-sea-banner.jpg"));
+        }
+        createListView(attractionModelArrayList2, "جنگل حاشیه‌ی دریای خزر");
+
 
         init();
     }
 
-    @Override
-    public void onDestroy() {
-        horizontalListView.setAdapter(null);
-        super.onDestroy();
+    private void createListView(ArrayList<AttractionModel> attractionModels, String title) {
+        View attractionListView1 = layoutInflater.inflate(R.layout.attraction_list_view, null);
+        HorizontalListView horizontalListView = attractionListView1.findViewById(R.id.HorizontalListView);
+        horizontalListView.setAdapter(new HorizontalAdapter(this, attractionModels));
+        LinearLayout linearLayout = attractionListView1.findViewById(R.id.linearLayout);
+        TextView titleTextView = linearLayout.findViewById(R.id.title);
+        titleTextView.setText(title);
+
+        linearLayout = findViewById(R.id.mainListView);
+        linearLayout.addView(attractionListView1);
     }
 
-    private void updateData() {
-        attractionModelArrayList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            attractionModelArrayList.add(new AttractionModel("جنگل سی سنگان نوشهر",
-                    "https://www.zistonline.com/uploadFiles/editor/Alangdareh-park.jpg"));
+    @Override
+    public void onDestroy() {
+        for (HorizontalListView horizontalListView : horizontalListViews) {
+            horizontalListView.setAdapter(null);
         }
+        super.onDestroy();
     }
 
     private void init() {
@@ -62,10 +82,9 @@ public class TourismAttractionActivity extends AppCompatActivity {
 
         mPager = findViewById(R.id.pager);
         mPager.setAdapter(new SliderAdapter(this, urls));
-        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        CircleIndicator indicator = findViewById(R.id.indicator);
         indicator.setViewPager(mPager);
 
-        // Auto start of viewpager
         final Handler handler = new Handler();
         final Runnable Update = new Runnable() {
             public void run() {
@@ -81,6 +100,6 @@ public class TourismAttractionActivity extends AppCompatActivity {
             public void run() {
                 handler.post(Update);
             }
-        }, 3000, 3000);
+        }, 0, 3000);
     }
 }
